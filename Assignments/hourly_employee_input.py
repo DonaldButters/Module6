66b0849c0dbca43deb5c3cd49171bc24a2b050e0
DonaldButters <70549495+DonaldButters@users.noreply.github.com>
2020-10-05 14:03:09 -0500

    Basic Function

--- Assignments/hourly_employee_input.py
+++ Assignments/hourly_employee_input.py
@@ -10,21 +10,25 @@ The purpose of this program is prompt a name, age, and hourly pay.
 Then the program out prints the input as a string
 
 """
+
 def get_user_input(): # keyword def with function name then ():
     """Prompts the user for name, age and prints a message"""  # docstring
-name = input("Please enter your name:")  # user input string
-age = int(input('Please enter your age. :')) # user input int
-if age in range(1,120): # handle the bad
-    print("Thank you")
-else:
-    input("Please enter your age between 1-120. :")
+    name = input("Please enter your name:")  # user input string
+    age = int(input('Please enter your age. :')) # user input int
+    if age in range(1,120): # handle the bad
+        print("Thank you")
+    else:
+        input("Please enter your age between 1-120. :")
+    pay_rate = float(input('Please enter your hourly pay rate. :')) # user inputs pay rate
+    if pay_rate in range(7,500): # handle the bad
+         print("Thank you")
+    else:
+        input("Please enter your pay rate between 7 and 500. :")
+    return 'Name:',name,'Age:',age,'Rate:',pay_rate, # Prints the inputs as a string
+from Assignments import weekly_pay
+print(get_user_input())
 
-rate = float(input('Please enter your hourly pay rate. :')) # user inputs pay rate
-if rate in range(7,500): # handle the bad
-    print("Thank you")
-else:
-    input("Please enter your pay rate between 7 and 500. :")
-print('Name:',name,'\nAge:',age,'\nRate:',rate, sep="\n") # Prints the inputs as a string
-
-if __name__ == '__main__':
+if __name__ == 'main':
     get_user_input()  # function call
+
+
---
+++ Assignments/weekly_pay.py
@@ -0,0 +1,8 @@
+from Assignments import hourly_employee_input
+def weekly_pay():
+    hours_worked = int(input('Please enter your work hours. :'))
+    hourly_pay_rate = int(input('Please re-enter your hourly pay rate :'))
+    weekly_pay=(hours_worked * hourly_pay_rate)
+    return weekly_pay
+print('Your pay this week is', weekly_pay())
+
