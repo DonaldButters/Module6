eed58120fb6c02550e06ef366f7a8f1a65f3083d
DonaldButters <70549495+DonaldButters@users.noreply.github.com>
2020-10-05 19:26:37 -0500

    Function Values

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -1,3 +1,22 @@
+"""
+
+Program: string_functions.py
+
+Author: Donald Butters
+
+Last date modified: 10/5/2020
+
+The purpose of this program is to ask for a name and lucky number
+whe the correct input is put in it will print out That is a lucky number
+and then print the persons name the same number as their lucky number
+
+:param parameter_1: User inputs a name
+:param Parameter_2: User inputs a luck number
+:returns Prints the persons name the number of the luck number
+:raises keyError: asks for the input again
+
+
+"""
 def multiply_string():
     x = input('Enter your name: ')
     n = int(input('Enter your lucky number between 1-9:'))
